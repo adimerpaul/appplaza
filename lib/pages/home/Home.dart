370f1acc4e159f6ac7b77615ba8558064f2fbe7c
adimer <adimer101@gmail.com>
2025-03-07 21:09:49 -0400

    colcoado caroucel

--- lib/pages/home/Home.dart
+++ lib/pages/home/Home.dart
@@ -1,3 +1,4 @@
+import 'package:carousel_slider/carousel_slider.dart';
 import 'package:flutter/material.dart';
 
 import '../../colors/Color.dart';
@@ -10,6 +11,12 @@ class Home extends StatefulWidget {
 }
 
 class _HomeState extends State<Home> {
+  int _currentIndex = 0;
+  List<String> _carouselImages = [
+    'assets/images/caroucel1.png',
+    'assets/images/caroucel2.png',
+    'assets/images/caroucel3.png',
+  ];
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -70,6 +77,62 @@ class _HomeState extends State<Home> {
                   ),
                 ),
               ],
+            ),
+            CarouselSlider(
+              items: _carouselImages.map((i) {
+                return Builder(
+                  builder: (BuildContext context) {
+                    return Container(
+                      width: MediaQuery.of(context).size.width,
+                      margin: EdgeInsets.symmetric(horizontal: 5.0),
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(5), // Esquinas redondeadas opcionales
+                        color: Colors.amber,
+                      ),
+                      child: ClipRRect(
+                        borderRadius: BorderRadius.circular(5), // Redondeo opcional
+                        child: Image.asset(
+                          i,
+                          fit: BoxFit.cover, // 🔥 Asegura que la imagen se recorte correctamente
+                          width: double.infinity, // Ajusta al ancho del contenedor
+                          height: 180, // Altura del carrusel
+                        ),
+                      ),
+                    );
+                  },
+                );
+              }).toList(),
+              options: CarouselOptions(
+                height: 180,
+                autoPlay: true,
+                autoPlayInterval: Duration(seconds: 3),
+                autoPlayAnimationDuration: Duration(milliseconds: 800),
+                autoPlayCurve: Curves.fastOutSlowIn,
+                enlargeCenterPage: true,
+                onPageChanged: (index, reason) {
+                  setState(() {
+                    _currentIndex = index;
+                  });
+                },
+              ),
+            ),
+            SizedBox(height: 10),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: _carouselImages.map((url) {
+                int index = _carouselImages.indexOf(url);
+                return Container(
+                  width: 8.0,
+                  height: 8.0,
+                  margin: EdgeInsets.symmetric(vertical: 10.0, horizontal: 2.0),
+                  decoration: BoxDecoration(
+                    shape: BoxShape.circle,
+                    color: _currentIndex == index
+                        ? Colors.white
+                        : Colors.white.withOpacity(0.4),
+                  ),
+                );
+              }).toList(),
             ),
             Text('Home Page',
                 style: TextStyle(
